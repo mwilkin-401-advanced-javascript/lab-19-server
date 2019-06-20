85899ee507a9d1bb3637d8cdffd84437b42d37bf
Matt Wilkin <mwilkin22@gmail.com>
2019-06-20 16:05:22 -0700

    add env, env-sample, and more working in server.

--- server.js
+++ server.js
@@ -1,2 +1,15 @@
 'use strict';
 
+const Q = require('@nmq/q/server');
+
+Q.start();
+
+const db = new Q('database');
+db.monitorEvent('create');
+db.monitorEvent('read');
+db.monitorEvent('update');
+db.monitorEvent('delete');
+
+const network = new Q('network');
+network.monitorEvent('file-save');
+network.monitorEvent('file-error');
